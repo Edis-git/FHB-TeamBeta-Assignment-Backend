c8bed6f8e466570de2906dfe27fe2163887a5b3d
Edis-git <149195946+Edis-git@users.noreply.github.com>
2024-06-23 20:01:45 +0200

    test.txt

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,1 @@
-export default {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  transform: {
-    "^.+\\.jsx?$": "babel-jest",
-    "^.+\\.tsx?$": "ts-jest",
-  },
-  moduleFileExtensions: ["js", "jsx", "ts", "tsx", "json", "node"],
-  globals: {
-    "ts-jest": {
-      useESM: true,
-    },
-  },
-};
+
